8b14d54ecfcfcec3e74e508052690eae41f9d9c6
Ujwal Trivedi <ujwaltrivedi@gmail.com>
2020-03-14 00:35:16 -0400

    code formatting

--- src/gitreleased.js
+++ src/gitreleased.js
@@ -1,43 +1,47 @@
-const git = require('simple-git');
+const git = require("simple-git");
 
-module.exports = function(repoName, repoBranch, callback){
-	var countItem = 0;
-	var countRelease = 0;
-	var json = {}
-	json.repository = repoName;
-	json.branch = repoBranch;
-	json.release = [];
-	var funcx = function(json){
-  		callback(json);
-	};
+module.exports = function(repoName, repoBranch, callback) {
+  var countItem = 0;
+  var countRelease = 0;
+  var json = {};
+  json.repository = repoName;
+  json.branch = repoBranch;
+  json.release = [];
+  var funcx = function(json) {
+    callback(json);
+  };
 
-	git(repoName).tags((err, update) =>{
-	  	if(update['latest']){
-	    	var dict = {};
-	    	dict[update['all'][update['all'].length-2]+".."+update['latest']] = null;
-			//console.log(dict);
+  git(repoName).tags((err, update) => {
+    if (update["latest"]) {
+      var dict = {};
+      dict[
+        update["all"][update["all"].length - 2] + ".." + update["latest"]
+      ] = null;
+      //console.log(dict);
 
-			json.release[countRelease] = {};
-			json.release[countRelease].start = update['all'][update['all'].length-2];
-			json.release[countRelease].end = update['latest'];
-			json.release[countRelease].earliest = '';
-			json.release[countRelease].latest = '';
-			json.release[countRelease].item = [];
+      json.release[countRelease] = {};
+      json.release[countRelease].start =
+        update["all"][update["all"].length - 2];
+      json.release[countRelease].end = update["latest"];
+      json.release[countRelease].earliest = "";
+      json.release[countRelease].latest = "";
+      json.release[countRelease].item = [];
 
-	    	git(repoName).log(dict, (err, update) => {
-	        	update['all'].forEach(element => {
-	            	json.release[countRelease].item[countItem] = {};
-	            	json.release[countRelease].item[countItem].message = element.message;
-	            	json.release[countRelease].item[countItem].hash = element.hash;
-	            	json.release[countRelease].item[countItem].timestamp = element.date;
-	            	json.release[countRelease].item[countItem].author = element.author_name;
-	            	countItem += 1;
-	        	});
-	        	callback(json);
-	    	});
-	    	//
-	  	}else{
-			callback(json);
-		}
-	});
-}
+      git(repoName).log(dict, (err, update) => {
+        update["all"].forEach(element => {
+          json.release[countRelease].item[countItem] = {};
+          json.release[countRelease].item[countItem].message = element.message;
+          json.release[countRelease].item[countItem].hash = element.hash;
+          json.release[countRelease].item[countItem].timestamp = element.date;
+          json.release[countRelease].item[countItem].author =
+            element.author_name;
+          countItem += 1;
+        });
+        callback(json);
+      });
+      //
+    } else {
+      callback(json);
+    }
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,34 +1,28 @@
 #!/usr/bin/env node
 
-const git = require('simple-git');
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
-const readline = require('readline');
-const fs = require('fs');
-const path = require('path');
-const minimist = require('minimist')
-const parse = require('url-parse');
-const lineByLine = require('n-readlines');
-const Promise = require('promise');
-const gitpending = require('./gitpending.js');
-const gitreleased = require('./gitreleased.js');
-
-
+const git = require("simple-git");
+const chalk = require("chalk");
+const clear = require("clear");
+const figlet = require("figlet");
+const readline = require("readline");
+const fs = require("fs");
+const path = require("path");
+const minimist = require("minimist");
+const parse = require("url-parse");
+const lineByLine = require("n-readlines");
+const Promise = require("promise");
+const gitpending = require("./gitpending.js");
+const gitreleased = require("./gitreleased.js");
 
 clear();
 console.log(
-  chalk.green(
-    figlet.textSync('Gitparser', { horizontalLayout: 'full' })
-  )
+  chalk.green(figlet.textSync("Gitparser", { horizontalLayout: "full" }))
 );
-console.log('\n');
+console.log("\n");
 //console.log(path.basename(process.cwd()));
-
 
 var argv = minimist(process.argv.slice(2));
 const usage = function() {
-
   const usageText = `
 Usage:
   gitparser <command> [options]
@@ -38,17 +32,16 @@ Commands:
 
 General Options:
   --repos <path>           default: repos.json - path to the repos file, one repo URL per line
-  `
-  console.log(chalk.white(usageText))
-}
+  `;
+  console.log(chalk.white(usageText));
+};
 
-
-if (!argv['repos']) {
+if (!argv["repos"]) {
   usage();
-  process.exit(1)
+  process.exit(1);
 }
 
-var filename = argv['repos'];
+var filename = argv["repos"];
 var reposList = new Array();
 let line;
 let lineNumber = 0;
@@ -67,157 +60,160 @@ var rr_repo_count = 0;
 
 //JSON.stringify(theJSON);
 
-var updateJSON = function(){
-
-}
+var updateJSON = function() {};
 //=============================================
 // should call repo processor 1 at a time
 var x = 0;
 function customAlert(jsonItem, callback) {
-	var repoName;
-	getRepoName(jsonItem.repo, (repoNameX)=>{ repoName = repoNameX; });
-    processRepoPromise = processRepo(jsonItem.repo, repoName, jsonItem.branch);
-	processRepoPromise.then(function(result){
-		console.log(result);
-		gitPendingPromise = getLogPendingReviews(repoName, jsonItem.branch);
-		gitReleasedPromise = getLogReleasedReviews(repoName, jsonItem.branch);
+  var repoName;
+  getRepoName(jsonItem.repo, repoNameX => {
+    repoName = repoNameX;
+  });
+  processRepoPromise = processRepo(jsonItem.repo, repoName, jsonItem.branch);
+  processRepoPromise.then(
+    function(result) {
+      console.log(result);
+      gitPendingPromise = getLogPendingReviews(repoName, jsonItem.branch);
+      gitReleasedPromise = getLogReleasedReviews(repoName, jsonItem.branch);
 
-		Promise.all([
-			Promise.resolve(gitPendingPromise),
-			Promise.resolve(gitReleasedPromise)
-		]).then(function(values){
-  			callback(values[0], values[1]);
-		});
-
-	}, function(err){
-		console.log(err);
-	});
+      Promise.all([
+        Promise.resolve(gitPendingPromise),
+        Promise.resolve(gitReleasedPromise)
+      ]).then(function(values) {
+        callback(values[0], values[1]);
+      });
+    },
+    function(err) {
+      console.log(err);
+    }
+  );
 }
 
 var loopArray = function(arr) {
-    customAlert(arr[x], function(pendingJson, releasedJson){
-        // set x to next item
-        x++;
+  customAlert(arr[x], function(pendingJson, releasedJson) {
+    // set x to next item
+    x++;
 
+    //console.log(pendingJson);
+    //console.log(releasedJson);
 
-		//console.log(pendingJson);
-		//console.log(releasedJson);
+    theJSON.pending_review[pr_repo_count] = pendingJson;
+    theJSON.released[rr_repo_count] = releasedJson;
+    pr_repo_count++;
+    rr_repo_count++;
 
-        theJSON.pending_review[pr_repo_count] = pendingJson;
-		theJSON.released[rr_repo_count] = releasedJson;
-        pr_repo_count++;
-		rr_repo_count++;
+    if (x == arr.length) {
+      //console.log(JSON.stringify(theJSON));
+      writeJsonToFile(JSON.stringify(theJSON));
+    }
 
-
-        if(x == arr.length){
-          //console.log(JSON.stringify(theJSON));
-          writeJsonToFile(JSON.stringify(theJSON));
-        }
-
-        // any more items in array? continue loop
-        if(x < arr.length) {
-            loopArray(arr);
-        }
-    });
-
-}
+    // any more items in array? continue loop
+    if (x < arr.length) {
+      loopArray(arr);
+    }
+  });
+};
 loopArray(jsonContent.repos);
-
 
 //=============================================
 
-function writeJsonToFile(json){
-  fs.writeFile('commits.json', json, function (err) {
-    console.log(chalk.blue('[Writing] commits to commits.json'));
+function writeJsonToFile(json) {
+  fs.writeFile("commits.json", json, function(err) {
+    console.log(chalk.blue("[Writing] commits to commits.json"));
     if (err) {
-      return console.log(err)
-    };
+      return console.log(err);
+    }
   });
 }
 
-function getRepoName(repoUrl, callback){
+function getRepoName(repoUrl, callback) {
   var q = parse(repoUrl, true);
   callback(path.basename(q.pathname).replace(".git", ""));
 }
 
-function changeBranch(repoName, repoBranch, callback){
-  console.log(chalk.green('[Changing] branch for repo '+repoName+' to ['+repoBranch+']'));
-  git(repoName).checkout(repoBranch, (err, update) =>{
-
+function changeBranch(repoName, repoBranch, callback) {
+  console.log(
+    chalk.green(
+      "[Changing] branch for repo " + repoName + " to [" + repoBranch + "]"
+    )
+  );
+  git(repoName).checkout(repoBranch, (err, update) => {
     callback(repoName, repoBranch);
   });
 }
 
 // download repos
-function downloadRepo(repoUrl, repoBranch, repoName){
+function downloadRepo(repoUrl, repoBranch, repoName) {
   return new Promise(function(resolve, reject) {
-      if (!fs.existsSync(repoName)) {
-          //
-          getRepoName(repoUrl, (repoPath) => {
-            console.log(chalk.blue('[Downloading] '+repoUrl))
-            git().clone(repoUrl, (err, update) =>{
-                if(!err){
-                  resolve(chalk.green('[Downloaded] '+repoUrl));
-                }else{
-                  reject(chalk.red('[Failed] '+err));
-                }
-              });
-          });
-          //
-      }else{resolve(chalk.green('[Repo Exists] '+repoName));}
+    if (!fs.existsSync(repoName)) {
+      //
+      getRepoName(repoUrl, repoPath => {
+        console.log(chalk.blue("[Downloading] " + repoUrl));
+        git().clone(repoUrl, (err, update) => {
+          if (!err) {
+            resolve(chalk.green("[Downloaded] " + repoUrl));
+          } else {
+            reject(chalk.red("[Failed] " + err));
+          }
+        });
+      });
+      //
+    } else {
+      resolve(chalk.green("[Repo Exists] " + repoName));
+    }
   });
 }
 
-
-function getLogPendingReviews(repoName, repoBranch){
-	console.log(chalk.magenta('[Downloading Pending Commits]'));
-	return new Promise(function(resolve, reject) {
-		var funcx = function(json){
-			resolve(json);
-		};
-		gitpending(repoName, repoBranch, funcx);
-	});
+function getLogPendingReviews(repoName, repoBranch) {
+  console.log(chalk.magenta("[Downloading Pending Commits]"));
+  return new Promise(function(resolve, reject) {
+    var funcx = function(json) {
+      resolve(json);
+    };
+    gitpending(repoName, repoBranch, funcx);
+  });
 }
 
-
-function getLogReleasedReviews(repoName, repoBranch){
-	console.log(chalk.magenta('[Downloading Released Commits]'));
-	return new Promise(function(resolve, reject) {
-		var funcx = function(json){
-		  resolve(json);
-		};
-		gitreleased(repoName, repoBranch, funcx);
-	});
+function getLogReleasedReviews(repoName, repoBranch) {
+  console.log(chalk.magenta("[Downloading Released Commits]"));
+  return new Promise(function(resolve, reject) {
+    var funcx = function(json) {
+      resolve(json);
+    };
+    gitreleased(repoName, repoBranch, funcx);
+  });
 }
-
 
 // process repos
 function processRepo(repoUrl, repoName, repoBranch) {
-	return new Promise(function(resolve, reject) {
-	    // does repo already exist?
-	    downloadRepoPromise = downloadRepo(repoUrl, repoBranch, repoName);
-	    downloadRepoPromise.then(function(result) {
-		     	console.log(result);
-		     	// get branch status
-		     	git(repoName).status((err, update) => {
-		         	if(!err){
-		             	// fetch all the remote branch
-		             	git(repoName).raw(['fetch','--all'], (err, result) => {
-		               	if(!err){
-		                  	// change branch after fetch
-		                  	changeBranch(repoName, repoBranch, (repoName, repoBranch) => {
-		                      	git(repoName).pull('origin', repoBranch, (err, result) => {
-									resolve(chalk.green('[Branch Switched] to '+repoBranch));
-								});
-		                  	});//
-		               	}
-		             	});
-		         	}else{
-					reject(chalk.red('[Failed] ' + err));
-		         	}
-		       });//
-	    },function(err) {
-			reject(chalk.red('[Failed] ' + err));
-	    });
-	});
+  return new Promise(function(resolve, reject) {
+    // does repo already exist?
+    downloadRepoPromise = downloadRepo(repoUrl, repoBranch, repoName);
+    downloadRepoPromise.then(
+      function(result) {
+        console.log(result);
+        // get branch status
+        git(repoName).status((err, update) => {
+          if (!err) {
+            // fetch all the remote branch
+            git(repoName).raw(["fetch", "--all"], (err, result) => {
+              if (!err) {
+                // change branch after fetch
+                changeBranch(repoName, repoBranch, (repoName, repoBranch) => {
+                  git(repoName).pull("origin", repoBranch, (err, result) => {
+                    resolve(chalk.green("[Branch Switched] to " + repoBranch));
+                  });
+                }); //
+              }
+            });
+          } else {
+            reject(chalk.red("[Failed] " + err));
+          }
+        }); //
+      },
+      function(err) {
+        reject(chalk.red("[Failed] " + err));
+      }
+    );
+  });
 }
